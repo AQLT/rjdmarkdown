d4196ae58b6ed2b250ca3229ebbd5de812a3211d
AQLT <alain.quartier@yahoo.fr>
2023-09-24 11:37:26 +0200

    create_rmd improvement

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
+# rjdmarkdown 0.2.2
+
+* correction in the create file of `create_rmd()`.
+
+* functions used in `create_rmd()` can be removed using a `NULL` value.
+
+
 # rjdmarkdown 0.2.1
 
 * `rjdmarkdown` now depends on `RJDemetra`.
--- R/create_rmd.R
+++ R/create_rmd.R
@@ -20,12 +20,21 @@ init_rmd <- function(title, knitr_chunk_opts){
 model_chunk <- function(model_name, preprocessing_fun = "print_preprocessing",
                         decomposition_fun = "print_decomposition",
                         diagnostics_fun = "print_diagnostics"){
-  c('', '```{r}',
-    sprintf(paste0(c(preprocessing_fun,
-                     decomposition_fun,
-                     diagnostics_fun),"(%s)"),
-            model_name),
-    '```', '')
+  vect <- c()
+  if (preprocessing_fun != "NULL")
+    vect <- c(vect, preprocessing_fun)
+  if (decomposition_fun != "NULL")
+    vect <- c(vect, decomposition_fun)
+  if (diagnostics_fun != "NULL")
+    vect <- c(vect, diagnostics_fun)
+  if (length(vect) > 0 ){
+    c('', '```{r}',
+      sprintf(paste0(vect, "(%s)"),
+              model_name),
+      '```', '')
+  } else {
+    ''
+  }
 }
 sa_item_chunk <- function(model_name, workspace_name){
   c('', '```{r}',
@@ -43,7 +52,7 @@ wk_chunk <- function(wk_name, i){
     '```', '')
 }
 
-#' Create and compile 'rmarkdown' file 
+#' Create and render 'rmarkdown' file 
 #'
 #' Function to create a 'rmarkdown' file with all the output and render it
 #' @param x the object to render: it can be a \code{"SA"}, \code{"jSA"}, \code{"sa_item"},
@@ -54,11 +63,14 @@ wk_chunk <- function(wk_name, i){
 #' a pdf output, \code{"html_document"} for a HTML output. 
 #' See \link[rmarkdown]{render} for more details.
 #' @param preprocessing_fun the function used to print the preprocessing. 
-#' \link{print_preprocessing} by default.
+#' \link{print_preprocessing} by default. 
+#' If \code{preprocessing_fun = NULL} the function is not used.
 #' @param decomposition_fun the function used to print the decomposition 
 #' \link{print_decomposition} by default.
+#' If \code{decomposition_fun = NULL} the function is not used.
 #' @param diagnostics_fun the function used to print the diagnostics 
 #' \link{print_diagnostics} by default.
+#' If \code{diagnostics_fun = NULL} the function is not used.
 #' @param title the title of the R Markdown document.
 #' @param knitr_chunk_opts options for R code chunks.
 #' See \link[knitr]{opts_chunk} for more details.
@@ -129,7 +141,7 @@ create_rmd.SA <- function(x,
                          preprocessing_fun = deparse(substitute(preprocessing_fun)),
                          decomposition_fun = deparse(substitute(decomposition_fun)),
                          diagnostics_fun = deparse(substitute(diagnostics_fun)))
-  
+  print(mod_obj)
   markobj <- c(init_rmd, mod_obj)
   
   writeLines(text = markobj,
@@ -183,6 +195,9 @@ create_rmd.workspace <- function(x,
   init_rmd <- init_rmd(title = title,
                        knitr_chunk_opts = deparse(substitute(knitr_chunk_opts)))
   wk_name <- deparse(substitute(x))
+  preprocessing_fun_ = deparse(substitute(preprocessing_fun))
+  decomposition_fun_ = deparse(substitute(decomposition_fun))
+  diagnostics_fun_ = deparse(substitute(diagnostics_fun))
   
   nb_mp <- RJDemetra::count(x)
   all_mp_obj <- lapply(seq_len(nb_mp),
@@ -190,13 +205,13 @@ create_rmd.workspace <- function(x,
                             extract_saitem <- wk_chunk(wk_name, i)
                             mp_tmp <- get_object(x, i)
                             mp_obj <- extract_mp_rmd(x = mp_tmp,
-                                                     section_level = "#",
                                                      mp_name = "jmultip_",
+                                                     section_level = "#",
                                                      workspace_name = wk_name,
-                                                     preprocessing_fun = deparse(substitute(preprocessing_fun)),
-                                                     decomposition_fun = deparse(substitute(decomposition_fun)),
-                                                     diagnostics_fun = deparse(substitute(diagnostics_fun)))
-                            c(extract_saitem,'',
+                                                     preprocessing_fun = preprocessing_fun_,
+                                                     decomposition_fun = decomposition_fun_,
+                                                     diagnostics_fun = diagnostics_fun_)
+                            c(extract_saitem, '',
                               mp_obj, '')
                           })
   all_mp_obj <- unlist(all_mp_obj)
@@ -312,7 +327,7 @@ extract_saitem_rmd <- function(sa_name, section_level = "#",
     sa_item_obj, 
     mod_obj,'')
 }
-extract_mp_rmd <- function(x,mp_name,
+extract_mp_rmd <- function(x, mp_name,
                            section_level = "#",
                            workspace_name,
                            preprocessing_fun,
@@ -331,7 +346,7 @@ extract_mp_rmd <- function(x,mp_name,
   c(sprintf("%s Multiprocessing '%s'",section_level, get_name(x)), '',
     all_sa_obj, '')
 }
-extract_mp_rmd_i <- function(i,x,mp_name,
+extract_mp_rmd_i <- function(i,x, mp_name,
                              section_level = "#",
                              workspace_name,
                              preprocessing_fun,
